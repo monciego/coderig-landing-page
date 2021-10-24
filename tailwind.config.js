90974a39c1c72243dcef5ff6c91d75beb5feb9c5
Jericho <jerichobantiquete@gmail.com>
2021-10-25 02:32:25 +0800

    created navbar

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  purge: ["./public/*.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "coderig-primary": "#2174EA",
+        "coderig-secondary": "#485668",
+        "coderig-indigo": "#5665F2",
+        "coderig-bg": "#F4F9FF",
+      },
+    },
+    fontFamily: {
+      Poppins: ["Poppins, sans-serif"],
+      FrankRuhlLibre: ["Frank Ruhl Libre, serif"],
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
