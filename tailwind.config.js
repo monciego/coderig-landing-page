3e463d63938415b41d2d3ef9bdf7e712e3efc408
Jericho <jerichobantiquete@gmail.com>
2021-11-23 05:47:53 +0800

    added jit mode and purge unused style for deployment / Optimizing for Production

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  // mode: "jit",
-  // purge: ["./public/**/*.html"],
-  purge: [],
+  mode: "jit",
+  purge: ["./public/**/*.html"],
+  // purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fill: {
